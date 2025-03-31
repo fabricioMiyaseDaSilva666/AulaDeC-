e666882e7a75deffd007c9a6cf23fb33c69dfb53
FABRICIO MIYASE DA SILVA <fabricio.msilva16@senacsp.edu.br>
2025-03-31 16:35:32 -0300

    terceiro commit

--- exerciciosTint/exerciciosTint/Control.cs
+++ exerciciosTint/exerciciosTint/Control.cs
@@ -18,18 +18,34 @@ namespace exerciciosTint
         public void mostrarMenu() 
         {
             Console.WriteLine("Escolha uma das opções abaixo: " +
-                               "\n0. Sair" +
-                               "\n1. Mostrar Números" +
-                               "\n2. Média de três notas" +
-                               "\n3. Número negativo, positivo ou zero" +
-                               "\n4. Tabuada" +
-                               "\n5. Número Fatorial" +
-                               "\n6. Ver a potêrncia e a raiz" +
-                               "\n7. Número par ou impar" +
-                               "\n8. Números em orndem crescente" +
-                               "\n9. Médias de um número" +
+                               "\n0.  Sair" +
+                               "\n1.  Mostrar Números" +
+                               "\n2.  Média de três notas" +
+                               "\n3.  Número negativo, positivo ou zero" +
+                               "\n4.  Tabuada" +
+                               "\n5.  Número Fatorial" +
+                               "\n6.  Ver a potêrncia e a raiz" +
+                               "\n7.  Número par ou impar" +
+                               "\n8.  Números em orndem crescente" +
+                               "\n9.  Médias de um número" +
                                "\n10. Vogais em uma palavra" +
-                               "\n11. Palavrinha invertida");
+                               "\n11. Palavrinha invertida" +
+                               "\n12. Número primo" +
+                               "\n13. Somar e Multiplicar" +
+                               "\n14. Somar média de três números" +
+                               "\n15. Porcentagem de 15%" +
+                               "\n16. Porcentagem de 5% e de 50%" +
+                               "\n17. Quadrado de um número" +
+                               "\n18. m/s para km" +
+                               "\n19. Calcular o IMC de alguém" +
+                               "\n20. Calcular o desconto de 9%" +
+                               "\n21. Desconto de 7%" +
+                               "\n22. Calcular o volume da caixa" +
+                               "\n23. Desconto de 27%" +
+                               "\n24. Área de um trapézio" +
+                               "\n25. Media de notas com ponderada" +
+                               "\n26. Valor parcelado" +
+                               "\n27. Consumo de gasolina por kms");
         }//Fim do método
 
         public void menu()
@@ -157,6 +173,154 @@ namespace exerciciosTint
                         string palavra = Console.ReadLine();
 
                         Console.WriteLine(this.exer.palavraInver(palavra));
+                        break;
+
+                    case 12:
+                        Console.WriteLine("Digite um número");
+                        int numPrimo = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.primo(numPrimo));
+                        break;
+
+                    case 13:
+                        Console.WriteLine("Digite um número");
+                        int numSoma1 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Digite mais um número");
+                        int numSoma2 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.somaMult(numSoma1, numSoma2));
+                        break;
+
+                    case 14:
+                        Console.WriteLine("Digite um númro");
+                        int mad1 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Digite mais um número");
+                        int mad2 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Digite mais um númro");
+                        int mad3 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.mediaTresNum(mad1, mad2, mad3));
+                        break;
+
+                    case 15:
+                        Console.WriteLine("Informe um valor");
+                        int porcentage = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.porcentagemNum(porcentage));
+                        break;
+
+                    case 16:
+                        Console.WriteLine("Informe um número");
+                        int porcentagem = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.porcentagem2(porcentagem));
+                        break;
+
+                    case 17:
+                        Console.WriteLine("Informe um número");
+                        int quadrado1 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Informe outro número");
+                        int quadrado2 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.quadrado(quadrado1, quadrado2));
+                        break;
+
+                    case 18:
+                        Console.WriteLine("Informe um número");
+                        int ms = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.km(ms));
+                        break;
+
+                    case 19:
+                        Console.WriteLine("Informe uma altura");
+                        double altura = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe um peso");
+                        double peso = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.imc(altura, peso));
+                        break;
+
+                    case 20:
+                        Console.WriteLine("Informe um valor");
+                        double preso = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.desconto(preso));
+                        break;
+
+                    case 21:
+                        Console.WriteLine("Informe um número");
+                        double descontos = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.descontos(descontos));
+                        break;
+
+                    case 22:
+                        Console.WriteLine("Informe um comprimeto");
+                        double comprimento = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe largura");
+                        double largura = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe uma altura");
+                        double alturas = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.volumeCaixa(comprimento, largura, alturas));
+                        break;
+
+                    case 23:
+                        Console.WriteLine("Informe um valor");
+                        double presos = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.desconto3(presos));
+                        break;
+
+                    case 24:
+                        Console.WriteLine("Informe um valor");
+                        int baseMaior = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Informe outro valor");
+                        int baseMenor = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Informe mais um valor");
+                        int h = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.trapezio(baseMaior, baseMenor, h));
+                        break;
+
+                    case 25:
+                        Console.WriteLine("Informe a primeira nota entre 0 e 10");
+                        double notas1 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe a primeira nota entre 0 e 10");
+                        double notas2 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe a primeira nota entre 0 e 10");
+                        double notas3 = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.mediaPon(notas1, notas2, notas3));
+                        break;
+
+                    case 26:
+                        Console.WriteLine("Informe um valor");
+                        double presosPar = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.desconto4(presosPar));
+                        break;
+
+                    case 27:
+                        Console.WriteLine("Informe os km");
+                        double kms = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Informe os litros de gasolina");
+                        double litros = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.distancia(kms, litros));
                         break;
                     default:
                         Console.WriteLine("Opção escolhida não é válida!");
--- exerciciosTint/exerciciosTint/exerciciosIniciais.cs
+++ exerciciosTint/exerciciosTint/exerciciosIniciais.cs
@@ -156,14 +156,183 @@ namespace exerciciosTint
 
         public string primo(int numPrimo)
         {
-            int numePrimo = int.Parse(Console.ReadLine());
-            static bool ePrimo(int n)
+            if(numPrimo <= 1)
             {
-                if (n < 2)
+                return "Não é primo";
+            }
+            for(int i = 2; i <= Math.Sqrt(numPrimo); i++)
+            {
+                if(numPrimo % i == 0)
                 {
-                    return false;
+                    return "Não é primo";
                 }
             }
+            return "É primo";
+        }//Fim do método
+
+        public string somaMult(int numSoma1, int numSoma2)
+        {
+            int soma = 0;
+            soma = numSoma1 + numSoma2;
+            int mult = 0;
+            mult = soma * numSoma1;
+            return "A soma dos dois números é " + soma + " E a multiplicação dessa soma com o primeiro número é " + mult;
+        }//Fim do método
+
+        public string mediaTresNum(int mad1, int mad2, int mad3)
+        {
+            int media1 = 0;
+            media1 = (mad1 + mad2 + mad3) / 3;
+            
+            return "A média desses 3 médias é " + media1;
+        }//Fim do método
+
+        public string porcentagemNum(int porcentage)
+        {
+            int resposta = 0;
+            resposta = (porcentage * 15) / 100;
+            return "15% desse valor é " + resposta;
+        }//Fim do método
+
+        public string porcentagem2(int porcentagem)
+        {
+            int resposta1 = 0;
+            int resposta2 = 0;
+
+            resposta1 = (porcentagem * 5) / 100;
+            resposta2 = (porcentagem * 50) / 100;
+
+            return "5% desse valor é " + resposta1 + " e 50% desse valor é " + resposta2;
+        }//Fim do método
+        
+        public string quadrado(int quadrado1, int quadrado2)
+        {
+            int resposta1 = 0;
+            int resposta2 = 0;
+            int resposta3 = 0;
+
+            resposta1 = quadrado1 * quadrado1;
+            resposta2 = quadrado2 * quadrado2;
+            resposta3 = resposta1 + resposta2;
+
+            return "O quadrado do primeiro número é " + resposta1 + " O segundo é " + resposta2 + "\n" + "e a soma desses quadrados é " + resposta3;
+        }//Fim do método
+
+        public string km(int ms)
+        {
+            double km = 0;
+
+            km = ms * 3.6;
+
+            return "Os km desse m/s são: " + km;
+        }//Fim do método
+
+        public string imc(double altura, double peso)
+        {
+            double imc = 0;
+            double alt = 0;
+
+            alt = altura * 2;
+            imc = altura / peso;
+
+            return "O IMC dessa pessoa é: " + imc;
+        }//Fim do método
+
+        public string desconto(double preso)
+        {
+            double desconto = 0;
+            double descontinho = 0;
+
+            desconto = (preso * 9) / 100;
+            descontinho = preso - desconto;
+
+            return "O desconto de 9% do seu item vai ser " + descontinho;
+        }//Fim do método
+
+        public string descontos(double descontos)
+        {
+            double desconto = 0;
+            double descontinho = 0;
+
+            desconto = (descontos * 7) / 100;
+            descontinho = descontos - desconto;
+
+            return "O valor original do seu produto é: " + descontos + "\n" + " O desconto de 7% do seu item vai ser " + descontinho;
+        }//Fim do método
+
+        public string volumeCaixa(double comprimento, double largura, double alturas)
+        {
+            double volume = 0;
+            volume = comprimento * largura * alturas;
+
+            return "O volume da caixa é " + volume;
+        }//Fim do método
+
+        public string desconto3(double presos)
+        {
+            double desconto = 0;
+            double descontinho = 0;
+
+            desconto = (presos * 27) / 100;
+            descontinho = presos - desconto;
+
+            return "O desconto de 27% do seu item vai ser " + descontinho;
+        }//Fim do método
+
+        public string trapezio(int baseMaior, int baseMenor, int h)
+        {
+            double resposta = 0;
+            resposta = (baseMaior + baseMenor) * h;
+            resposta = resposta / 2;
+
+            return "O trapézio desses valores é " + resposta;
+        }//Fim do método
+
+        public string mediaPon(double notas1, double notas2, double notas3)
+        {
+            double media = 0;
+            double mediaFinal = 0;
+            double notaPon1 = 0;
+            double notaPon2 = 0;
+            double notaPon3 = 0;
+
+            notaPon1 = notas1 * 2;
+            notaPon2 = notas2 * 3;
+            notaPon3 = notas3 * 5;
+            media = notaPon1 + notaPon2 + notaPon3;
+            mediaFinal = media / 10;
+
+            if (mediaFinal == -1)
+            {
+                return "Uma ou mais notas estão erradas, digite novamente";
+            }
+            else
+            {
+                return "\n\n A média é: " + mediaFinal;
+            }
+
+        }//Fim do método
+
+        public string desconto4(double presosPar)
+        {
+            double desconto = 0;
+            double descontinho = 0;
+            double parcela = 0;
+
+            desconto = (presosPar * 16) / 100;
+            descontinho = presosPar + desconto;
+            parcela = descontinho / 10;
+
+            return "O acrescimo de 16% do seu item vai ser " + descontinho + " e sua parcela de 10 vezes vai sair por" + parcela;
+        }//Fim do método
+
+        public string distancia(double kms, double litros)
+        {
+            double consumoMedio = 0;
+
+            consumoMedio = kms / litros;
+
+            return "O consumo médio de gasolina é de" + consumoMedio + "km por litro";
         }
     }//Fim da classe
 }//Fim do projeto
